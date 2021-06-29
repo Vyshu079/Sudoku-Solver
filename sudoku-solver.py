c004b98202ae8de9002349697299c23474679bb5
Vyshu <74536717+Vyshu079@users.noreply.github.com>
2021-06-29 15:35:18 +0530

    Update sudoku-solver.py

--- sudoku-solver.py
+++ sudoku-solver.py
@@ -1,29 +1,29 @@
 # solver.py
-def solve(bo):
+def solver(board):
     """
     Solves a sudoku board using backtracking
     :param bo: 2d list of ints
     :return: solution
     """
-    find = find_empty(bo)
+    find = find_empty(board)
     if find:
         row, col = find
     else:
         return True
 
     for i in range(1,10):
-        if valid(bo, (row, col), i):
-            bo[row][col] = i
+        if valid(board, (row, col), i):
+            board[row][col] = i
 
-            if solve(bo):
+            if solver(board):
                 return True
 
-            bo[row][col] = 0
+            board[row][col] = 0
 
     return False
 
 
-def valid(bo, pos, num):
+def valid(board, pos, num):
     """
     Returns if the attempted move is valid
     :param bo: 2d list of ints
@@ -33,13 +33,13 @@ def valid(bo, pos, num):
     """
 
     # Check row
-    for i in range(0, len(bo)):
-        if bo[pos[0]][i] == num and pos[1] != i:
+    for i in range(0, len(board)):
+        if board[pos[0]][i] == num and pos[1] != i:
             return False
 
     # Check Col
-    for i in range(0, len(bo)):
-        if bo[i][pos[1]] == num and pos[1] != i:
+    for i in range(0, len(board)):
+        if board[i][pos[1]] == num and pos[1] != i:
             return False
 
     # Check box
@@ -49,42 +49,42 @@ def valid(bo, pos, num):
 
     for i in range(box_y*3, box_y*3 + 3):
         for j in range(box_x*3, box_x*3 + 3):
-            if bo[i][j] == num and (i,j) != pos:
+            if board[i][j] == num and (i,j) != pos:
                 return False
 
     return True
 
 
-def find_empty(bo):
+def find_empty(board):
     """
     finds an empty space in the board
     :param bo: partially complete board
     :return: (int, int) row col
     """
 
-    for i in range(len(bo)):
-        for j in range(len(bo[0])):
-            if bo[i][j] == 0:
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
                 return (i, j)
 
     return None
 
 
-def print_board(bo):
+def print_board(board):
     """
     prints the board
     :param bo: 2d List of ints
     :return: None
     """
-    for i in range(len(bo)):
+    for i in range(len(board)):
         if i % 3 == 0 and i != 0:
             print("- - - - - - - - - - - - - -")
-        for j in range(len(bo[0])):
+        for j in range(len(board[0])):
             if j % 3 == 0:
                 print(" | ",end="")
 
             if j == 8:
-                print(bo[i][j], end="\n")
+                print(boaqrd[i][j], end="\n")
             else:
-                print(str(bo[i][j]) + " ", end="")
+                print(str(board[i][j]) + " ", end="")
 
